169c4b30eca93291539135bca688fbc343b45ed2
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-28 09:07:51 +0330

    feat:

--- packages/server/src/models/global.ts
+++ packages/server/src/models/global.ts
@@ -1,7 +1,7 @@
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Express {
-        interface User extends User {}
+        interface User {}
     }
 }
 
